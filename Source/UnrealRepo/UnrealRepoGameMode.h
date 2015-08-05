9b93ab12317036e49477b177161592573c213185
PedroTrujilloV <pedrot211@gmail.com>
2015-08-05 01:53:04 -0400

    Get start repo

---
+++ Source/UnrealRepo/UnrealRepoGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UnrealRepoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALREPO_API AUnrealRepoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
